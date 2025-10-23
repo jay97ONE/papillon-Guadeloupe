6f78d1176cf047adbdc9c20ffeebc0c550a7c590
jay97ONE <kgminfinity@gmail.com>
2025-10-23 02:48:20 +0200

    Update Code-component-135-60.tsx

--- Code-component-135-60.tsx
+++ Code-component-135-60.tsx
@@ -1,20 +1,16 @@
-MIT License
+import React from "react";
 
-Copyright (c) 2025 PAPILLON GUADELOUPE SASU
+type Props = {
+  className?: string;
+  message?: string;
+};
 
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
+const CodeComponent13560: React.FC<Props> = ({ className = "", message = "Component 135-60" }) => {
+  return (
+    <section className={`p-4 rounded shadow-sm bg-gray-50 ${className}`}>
+      <p className="text-sm text-gray-700">{message}</p>
+    </section>
+  );
+};
 
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
-INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
-PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
-HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
-OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
-SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+export default CodeComponent13560;
