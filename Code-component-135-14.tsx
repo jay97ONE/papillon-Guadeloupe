469816780d9fad06ec67aacd52eccb00e89bbd3e
jay97ONE <kgminfinity@gmail.com>
2025-10-23 02:47:00 +0200

    Update Code-component-135-14.tsx

--- Code-component-135-14.tsx
+++ Code-component-135-14.tsx
@@ -1,20 +1,18 @@
-MIT License
+import React from "react";
 
-Copyright (c) 2025 PAPILLON GUADELOUPE SASU
+type Props = {
+  className?: string;
+  children?: React.ReactNode;
+};
 
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
+const CodeComponent13514: React.FC<Props> = ({ className = "", children }) => {
+  return (
+    <div className={`p-4 bg-white text-slate-900 ${className}`}>
+      <pre className="whitespace-pre-wrap text-sm">{children ?? "Component 135-14"}</pre>
+    </div>
+  );
+};
 
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
+export default CodeComponent13514;
 
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
-INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
-PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
-HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
-OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
-SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
